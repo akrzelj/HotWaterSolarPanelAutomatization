d95f58014d29d16e41066705d94cb39c0f793b63
etnaetna <akrzel00@fesb.hr>
2019-04-25 11:28:52 +0200

    Changed arduino program to fetch temperatures by probe addres
    Added script for fetching probe address
    Added txt file with probe addresses (with weird distinction kesa-no
    kesa)

--- main/main.ino
+++ main/main.ino
@@ -1,42 +1,46 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
- 
-// Data wire is plugged into pin 2 on the Arduino
+
+// Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 2
- 
-// Setup a oneWire instance to communicate with any OneWire devices 
-// (not just Maxim/Dallas temperature ICs)
+
+// Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(ONE_WIRE_BUS);
- 
+
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
- 
+
+// Addresses of 3 DS18B20s
+uint8_t sensor1[8] = { 0x28, 0xFF, 0x9A, 0xF3, 0x94, 0x16, 0x04, 0x2A }; //kesa
+uint8_t sensor2[8] = { 0x28, 0xFF, 0x6D, 0x14, 0xA0, 0x16, 0x05, 0x0C };
+
 void setup(void)
 {
-  // start serial port
   Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
-
-  // Start up the library
   sensors.begin();
 }
- 
- 
+
 void loop(void)
 {
-  // call sensors.requestTemperatures() to issue a global temperature
-  // request to all devices on the bus
-  Serial.print(" Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
-
-  Serial.print("Temperature 1 is: ");
-  Serial.println(sensors.getTempCByIndex(0)); // Why "byIndex"? 
-    // You can have more than one IC on the same bus. 
-    // 0 refers to the first IC on the wire
-   
-    Serial.print("Temperature 2 is: ");
-  Serial.println(sensors.getTempCByIndex(1)); // Why "byIndex"? 
+  sensors.requestTemperatures();
   
-    delay(1000);
+  Serial.print("Sensor 1 [kesa]: ");
+  printTemperature(sensor1);
+  
+  Serial.print("Sensor 2 [van kese]: ");
+  printTemperature(sensor2);
+  
+  Serial.println();
+  delay(1000);
+}
+
+void printTemperature(DeviceAddress deviceAddress)
+{
+  float tempC = sensors.getTempC(deviceAddress);
+  Serial.print(tempC);
+  Serial.print((char)176);
+  Serial.print("C  |  ");
+  Serial.print(DallasTemperature::toFahrenheit(tempC));
+  Serial.print((char)176);
+  Serial.println("F");
 }
